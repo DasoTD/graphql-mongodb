73171dea6e3b98c3552c92bb5292e1467a604378
David Daso <daso.timileyin@gmail.com>
2022-07-22 12:28:45 +0100

    dockerized

---
+++ test.js
@@ -0,0 +1,24 @@
+function dd() {
+  const number = 100;
+  let res;
+  for (let i = 0; i < number.length; i++) {
+    const val = number[i];
+    console.log(val);
+    
+  }
+  //console.log(res);
+}
+// if (val % 3 == 0) {
+    //   res = 'Fizz';
+    // }
+    // if (val % 5 == 0) {
+    //   res = 'bizz';
+    // }
+    // let mult = 3 * 5;
+    // if (val % mult == 0) {
+    //   res = 'fizzbizz';
+    // } else {
+    //   res = val;
+    // }
+
+     // res;
